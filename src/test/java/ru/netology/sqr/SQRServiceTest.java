61913ee8b97fd99eadc43b2cee4558736ebff12b
Valeriya <tmcxezsim5nn@mail.ru>
2021-09-23 16:57:33 +0300

    Initial commit

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+
+    @Test
+
+    public void shouldSqrNumber() {
+        SQRService sqrService = new SQRService();
+        int expected = 3;
+        int actual = sqrService.sqrNumber(200, 300);
+        assertEquals(expected, actual);
+    }
+}
+
